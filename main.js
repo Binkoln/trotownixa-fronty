9e3ca40d4cf31e2c659d90d0513991f907a8b6c8
bartko44 <111601258+bartko44@users.noreply.github.com>
2023-12-05 19:13:23 +0100

    selectmesh, stldecode

--- main.js
+++ main.js
@@ -4,16 +4,9 @@ import * as GUI from '@babylonjs/gui/2D';
 import '@babylonjs/loaders/STL';
 
 const canvas = document.getElementById('renderCanvas');
-const engine = new BABYLON.Engine(canvas);
+const engine = new BABYLON.Engine(canvas, true);
+let jsonData, selectedMesh = null, stlName='cake.stl';
 
-const createScene = async function() {
-  const scene = new BABYLON.Scene(engine);
-  scene.clearColor = new BABYLON.Color3(0.1, 0.2, 0.2);
-  scene.createDefaultCameraOrLight(true,false,true);
-  scene.activeCamera.position = new BABYLON.Vector3(50, 50, 50); 
-
-  let stlName = 'cake.stl';
-  
 const loadMesh = () => {   
     BABYLON.SceneLoader.ImportMesh(
       '',
@@ -23,17 +16,60 @@ const loadMesh = () => {
     );
   }
 
-const removeMesh = () => {
-    scene.getMeshByName('stlmesh').dispose()
-}
-
-const addLayer = () =>{
-  fetch("http://localhost:8888/addLayer")
-  .then((response) => response.json())
-  .then((json) => console.log(json));
-}
+  const addLayer = async () => {
+    await fetch("http://localhost:8888/addLayer")
+      .then((response) => response.json())
+      .then((json) => {
+        jsonData = json;
+      })
+      .catch((error) => {
+        console.error('Wystąpił błąd:', error);
+      });
+      const jsonDataString = JSON.stringify(jsonData);
+      const valuesArray = Object.values(JSON.parse(jsonDataString));
   
-//buttons
+  const objDataURL = "data:;base64," + valuesArray;
+  BABYLON.SceneLoader.Append('', objDataURL, scene, undefined, undefined, undefined, ".stl");
+  };
+
+  const highlightMesh = (mesh) => {
+    if (!mesh.material) {
+      mesh.material = new BABYLON.StandardMaterial('highlightMaterial', scene);
+    }
+
+    // Zmiana koloru 
+    mesh.material.diffuseColor = new BABYLON.Color3(0, 1, 0);
+  }
+
+  const handleMouseClickOnMesh = (event) => {
+
+    const pickResult = scene.pick(event.clientX, event.clientY);
+      if (pickResult && pickResult.hit) {
+      // Anuluj zaznaczenie
+      if (selectedMesh) {
+        selectedMesh.material = new BABYLON.StandardMaterial('whiteMaterial', scene);
+        selectedMesh.material.diffuseColor = new BABYLON.Color3(1, 1, 1); // Biały kolor
+        }
+  
+        // Zaznacz nowy mesh
+        selectedMesh = pickResult.pickedMesh;
+          highlightMesh(selectedMesh);
+  
+          console.log('Zaznaczony mesh:', selectedMesh.name);
+      }
+    }
+
+  const removeMesh = () => {
+      scene.getMeshByName('stlmesh').dispose()
+  }
+  
+const createScene = async function() {
+  const scene = new BABYLON.Scene(engine);
+  scene.clearColor = new BABYLON.Color3(0.1, 0.2, 0.2);
+  scene.createDefaultCameraOrLight(true,false,true);
+  scene.activeCamera.position = new BABYLON.Vector3(50, 50, 50); 
+  
+//gui
 let advancedTexture = GUI.AdvancedDynamicTexture.CreateFullscreenUI("GUI", true, scene);
 //advancedTexture.parseFromURLAsync("guiTexture.json");
 let loadedGUI = await advancedTexture.parseFromSnippetAsync("7ID8B3#13");
@@ -46,6 +82,7 @@ const clearBtn = advancedTexture.getControlByName('clearBtn');
 loadBtn.onPointerClickObservable.add(loadMesh);
 addLayerBtn.onPointerClickObservable.add(addLayer);
 clearBtn.onPointerClickObservable.add(removeMesh);
+canvas.addEventListener('click', handleMouseClickOnMesh);
 
 //Inspector.Show(scene, {});
 
@@ -57,7 +94,6 @@ const scene = await createScene();
 engine.runRenderLoop(function(){
   scene.render();
 });
-
 
 window.addEventListener('resize',function(){
   engine.resize();
