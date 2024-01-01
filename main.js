3bb57873932220b88c7fdf2a85c50414ac265a66
bartko44 <111601258+bartko44@users.noreply.github.com>
2024-01-02 00:14:10 +0100

    setLayer func

--- main.js
+++ main.js
@@ -5,10 +5,10 @@ import '@babylonjs/loaders/STL';
 
 const canvas = document.getElementById('renderCanvas');
 const engine = new BABYLON.Engine(canvas, true);
-let jsonData, selectedMesh = null, stlName='cake.stl';
+let jsonData, selectedMesh = null, stlName='cake.stl', meshParams = {};
 
 const addLayer = async () => {
-  var requestData = { "color": "0xFF00FF", "type": "box"};
+  let requestData = { "color": "0xFF00FF", "type": "box"};
 
   await fetch("http://localhost:8888/add/layer", {
     method: "POST",
@@ -29,39 +29,39 @@ const object = BABYLON.SceneLoader.Append('', objDataURL, scene, undefined, unde
 };
 
 
+// dane mesha
+const getMeshData = (meshData) => {
+   
+  meshParams = {
+    scale: {
+      x: meshData.scaling.x,
+      y: meshData.scaling.y,
+      z: meshData.scaling.z
+    },
+    rotation: {
+      x: meshData.rotation.x,
+      y: meshData.rotation.y,
+      z: meshData.rotation.z
+    },
+    position: {
+      x: meshData.position.x,
+      y: meshData.position.y,
+      z: meshData.position.z
+    },
+    name: meshData.name
+    
+ };
 
-//okno z danymi mesha
-const showModal = (meshData) => {
-  const modal = document.createElement("div");
-  modal.className = "modal";
-  document.body.appendChild(modal);
-
-  const scaleInfo = document.createElement("p");
-  scaleInfo.innerHTML = "Skala (X,Y,Z):<br>" + meshData.scaling.x + "<br>" + meshData.scaling.y + "<br>" + meshData.scaling.z;
-  modal.appendChild(scaleInfo);
-  
-  const rotationInfo = document.createElement("p");
-  rotationInfo.innerHTML = "Rotacja (X,Y,Z):<br>" + meshData.rotation.x + "<br>" + meshData.rotation.y + "<br>" + meshData.rotation.z;
-  modal.appendChild(rotationInfo);
-
-  const positionInfo = document.createElement("p");
-  positionInfo.innerHTML = "Pozycja (X,Y,Z):<br>" + meshData.position.x + "<br>" + meshData.position.y + "<br>" + meshData.position.z;
-  modal.appendChild(positionInfo);
-
-  const nameInfo = document.createElement("p");
-  nameInfo.innerHTML = "Nazwa warstwy: " + meshData.name;
-  modal.appendChild(nameInfo);
-
-  
-  
 }
+  
+
 const setLayer = (meshData) =>{
+  
+  var jsonString = JSON.stringify(meshParams);
+  console.log(meshParams)
+  console.log(jsonString);
 
-  console.log('Ustawiania warstwy')
 }
-
-
-
 
 //laduje z pliku mesh
   const loadMesh = () => {   
@@ -86,11 +86,11 @@ const createScene = async function() {
   scene.clearColor = new BABYLON.Color3(0.1, 0.2, 0.2);
   scene.createDefaultCameraOrLight(true,false,true);
   scene.activeCamera.position = new BABYLON.Vector3(50, 50, 50); 
+
   
 //gui
 const advancedTexture = GUI.AdvancedDynamicTexture.CreateFullscreenUI("GUI", true, scene);
 const loadedGUI = await advancedTexture.parseFromSnippetAsync("7ID8B3#49");
-
 const addLayerBtn = advancedTexture.getControlByName('addLayerBtn');
 const setLayerBtn = advancedTexture.getControlByName('setLayerBtn');
 const addDecorationBtn = advancedTexture.getControlByName('addDecorationBtn');
@@ -111,6 +111,7 @@ sliders.forEach((slider) => {
   slider.minimum = minSliderValue;
   slider.maximum = maxSliderValue;
 });
+
 // skalowanie mesha
 const updateSliders = (mesh) => {
   if (mesh) {
@@ -139,7 +140,7 @@ const handleMouseClickOnMesh = (event) => {
       const pickedMesh = pickResult.pickedMesh;
       updateSliders(pickedMesh);
       
-      showModal({
+      getMeshData({
         scaling: pickedMesh.scaling,
         rotation: pickedMesh.rotation,
         position: pickedMesh.position,
@@ -170,19 +171,29 @@ const handleMouseClickOnMesh = (event) => {
 //events
 loadBtn.onPointerClickObservable.add(loadMesh);
 addLayerBtn.onPointerClickObservable.add(addLayer);
+setLayerBtn.onPointerClickObservable.add(setLayer);
 clearBtn.onPointerClickObservable.add(removeMesh);
 downloadBtn.onPointerClickObservable.add(downloadProject);
 addDecorationBtn.onPointerClickObservable.add(addDecoration);
-setLayerBtn.onPointerClickObservable.add(setLayer);
 canvas.addEventListener('click', handleMouseClickOnMesh);
-
-
-
-              
+colorPicker.onValueChangedObservable.add((value) => {
+  if (selectedMesh) {
+    // ustaw kolor mesha
+    selectedMesh.material.diffuseColor = new BABYLON.Color3(value.r, value.g, value.b);
+  }
+});
+           
 return scene;
 }
 
 const scene = await createScene();
+
+
+const gizmoManager = new BABYLON.GizmoManager(scene);
+gizmoManager.positionGizmoEnabled = true;
+gizmoManager.rotationGizmoEnabled = true;
+gizmoManager.scaleGizmoEnabled = true;
+gizmoManager.updateGizmoRotationToMatchAttachedMesh = false;
 
 engine.runRenderLoop(function(){
   scene.render();
